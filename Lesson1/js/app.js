3cd06a76fa0280ff531ece32666858c43912d58f
Coddy <yusupovjunior21@gmail.com>
2024-01-07 15:32:27 +0500

    Lesson-1

---
+++ Lesson1/js/app.js
@@ -0,0 +1,39 @@
+// "use strict"
+
+
+// user = 20;
+// console.log(user);
+
+
+
+// let number = 10;
+// const userName = "Yaxyoxon"
+// console.log(user)
+// var user = "Ulugbek"
+
+// console.log(user);
+// let user = 10;
+// const user = 10;
+// console.log(user);
+
+
+// let userName = "Abror"
+// console.log();
+
+// let num1 = 10;
+// let num2 = num1;
+
+
+
+// console.log(num1)
+// console.log(num2)
+
+// let user = 10;
+// user += 20;
+
+// console.log(user)
+
+// const user = 10;
+// user = 20;
+
+// console.log(user);
