440c808d6e1b6f24c9377b5de7eb28b7667a5a29
Coddy <yusupovjunior21@gmail.com>
2024-01-08 15:18:03 +0500

    lesson 2

---
+++ lesson2/js/app.js
@@ -0,0 +1,81 @@
+
+// 1 Boolean - Mantiqiy malumot turi;
+
+// let buRostmi = true;
+// console.log(buRostmi);
+
+// let isMarried = true;
+// console.log(typeof isMarried);
+
+// let yolgon = false;
+// console.log(yolgon);
+
+// 2 Null - Qiymat tayinlanmagan
+
+// let age = null;
+// console.log(age);
+
+// 3 Underfined - QIymati yo'q
+
+// let ism
+// console.log(ism);
+
+// 4 number - raqamli malumot
+
+// let raqam = 5.14;
+// console.log(raqam);
+// let num = 234;
+// console.log(typeof num);
+// 5 String - Simvollardan tashkil topgan malumot turi
+
+// let ism = "abror"
+// console.log(ism);
+// let ism = "doniyor"
+// console.log(typeof ism);
+
+// 6 Symbol - Unik nick
+
+// let nickName = Symbol (`id`)
+
+// 7 BigInt - Kattaroq sonlarni o'ziga oladi
+
+// let num = BigInt(23523985798237589236528936529365289365928365892365892359)
+
+// console.log(num);
+
+// let age = +prompt("Yoshingiz nechida ?" ,"" )
+// console.log(typeof age);
+
+// console.log(2+2+3*12)
+
+// window.document.body.style.backgroundColor = "red"
+
+// Object 
+
+
+// let  love = {
+//     height: 1.70,
+//     haircolor: "black",
+//     isshebeauty: true,
+//     isphone : {
+//         model: "iphone",
+//         versiya : 12,
+//     }
+
+// }
+
+// love.haircolor = "orange"
+// console.log(love.haircolor);
+
+
+//                  0       1     2       3     4     5    6   7
+// let mycolors = ["red", "blue", "pink", "black", 25, true, {}, []]
+ 
+// mycolors[1] = "green"
+
+// console.log(mycolors);
+
+// console.log(typeof 0);
+
+
+// document.write("salom")
