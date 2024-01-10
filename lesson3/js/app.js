4f5f903a13f9d19064114f2ae3ebe195e066f994
Coddy <yusupovjunior21@gmail.com>
2024-01-10 15:35:07 +0500

    Lesson3

---
+++ lesson3/js/app.js
@@ -0,0 +1,97 @@
+// let love = {}
+
+// let a = prompt("she is property"),
+//     b = prompt("properties value"),
+//     c = prompt("height"),
+//     d = prompt("value")
+
+
+
+// love[a] = b
+// love[c] = d
+
+// console.log(love);
+
+
+// interpolatsiya
+
+// let youtubeName = prompt("Youtube kanal kiriting")
+
+// console.log(`https://youtube.com/${youtubeName}/videos`);
+
+
+// console.log( false + false);
+
+
+// Math operators
+
+
+
+// console.log(44 / 22);
+
+// let num1 =  10;
+// let num2 = 10;
+// console.log(num1++);
+// console.log(num2--);
+
+// console.log(num1++);
+// console.log(num2--);
+// console.log(num1++);
+// console.log(num2--);
+// console.log(num1++);
+// console.log(num2--);
+
+// console.log(12.5 % 3);
+
+// let num = 10;
+// let num1 = 3;
+// let result = 10%3;
+// console.log(22 + result);
+
+// console.log(16 < 17);
+
+// console.log(true && true && true);
+
+// console.log(true || false || false);
+
+// let age = true;
+// let userName = true; 
+// let user = false;
+
+// console.log(age && userName && !user);
+
+
+// let age = !true;
+
+// console.log(age);
+
+// console.log(2 + 2 * 2 !== "6");
+
+// console.log(3 ** 3);
+
+// let a = +prompt("Son kiriting"),
+//     b = +prompt("Son kiriting")
+
+// let result = a + b 
+
+// console.log(result);
+
+// let numberOfSeries = +prompt("Nechta serial ko'rdingiz");
+
+// let seriesDB = {
+//     count : numberOfSeries,
+//     series: {},
+//     actors: {},
+//     genres: [],
+//     privat: false,
+// }
+
+// let a = prompt("Oxirgi ko'rgan serialingiz"),
+//     b = prompt("Necha baxo berasiz ?")
+//     c = prompt("Oxirgi ko'rgan serialingiz ?")
+//     d = prompt("Necha baxo berasiz ?")
+
+// seriesDB.series[a] = b
+// seriesDB.series[c] = d
+// console.log(seriesDB);
+
